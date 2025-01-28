a61627348ef0cf71ad9ae1bc569700b6e3567fbc
Melih Can AYDIN <melihcanaydin@gmail.com>
2025-01-29 00:31:04 +0200

    Add unit tests for Video Streaming API

---
+++ src/test/java/com/example/videostreamingapi/video_streaming_api/VideoStreamingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videostreamingapi.video_streaming_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
